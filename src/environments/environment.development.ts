e817ab6365a5d00c7c22bbe1352dddd199e4a591
Francisco Carrillo <fraescaya10@gmail.com>
2026-02-02 16:32:25 -0500

    Adding environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    apiUrl: "<LOCAL_API_URL>",
+    firebaseConfig: {
+        projectId: "<FIREBASE_PROJECT_ID>",
+        appId: "<FIREBASE_APP_ID>",
+        storageBucket: "<FIREBASE_STORAGE_BUCKER>",
+        apiKey: "<FIREBASE_API_KEY>",
+        authDomain: "<FIREBASE_AUTH_DOMAIN>",
+        messagingSenderId: "<FIREBASE_MESSAGE_SENDER_ID>"
+    },
+    useEmulators: false
+};
